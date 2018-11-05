3fcc0cd17388e8962f63365a2cb6e7b1065c999d
Christian <chrishyaka@yahoo.fr>
2018-11-05 13:05:07 +0000

    if , else if, else, and & or

---
+++ javascript/ex5.js
@@ -0,0 +1,9 @@
+let example = 5;
+// can change && by ||
+ if (example === 5  && true === true){
+     console.log('Runs');
+ } else if(false) {
+     console.log('else if ')
+ } else {
+     console.log('else')
+ }
