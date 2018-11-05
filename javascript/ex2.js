c6d2eecaea98762517fca725fd22fc9bd7db5614
Christian <chrishyaka@yahoo.fr>
2018-11-05 12:30:48 +0000

    Relational operator challenge

---
+++ javascript/ex2.js
@@ -0,0 +1,10 @@
+let example1 = 5 ===5;
+let example2 = 5 =='5';
+let example3 = 6 !='6';
+let example4 = 7 !=='7';
+
+console.log(example1);
+console.log(example2);
+console.log(example3);
+console.log(example4);
+
