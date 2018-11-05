1066e8d1af5ff8e29c18893798601ee3a9461be2
Christian <chrishyaka@yahoo.fr>
2018-11-05 13:43:31 +0000

    function

--- javascript/ex7.js
+++ javascript/ex7.js
@@ -2,7 +2,7 @@ let total = 0;
 
 let numArray = [10, 20, 30, 40, 50, 60, 70, 80];
 for (let i = 0; i <numArray.length; i++) {
-
+ 
     total += numArray[i];
 }
 console.log(total);
---
+++ javascript/ex9.js
@@ -0,0 +1,8 @@
+function add( num1, num2){
+    return num1 + num2;
+
+}
+
+console.log(add(10, 12)) ;
+console.log(add(34, 21)) ;
+console.log(add(5, 34)) ;
