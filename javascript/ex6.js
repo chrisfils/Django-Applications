bdffe79226820802d6ce25e904a5146379474916
Christian <chrishyaka@yahoo.fr>
2018-11-05 13:25:58 +0000

    Switch statement for ex6 and for loop for ex7

---
+++ javascript/ex6.js
@@ -0,0 +1,15 @@
+let studentAnswer = 'F';
+switch(studentAnswer) {
+    case 'A':
+        console.log("A is wrong");
+        break;
+    case 'B':
+        console.log("B is wrong");
+        break;
+    case 'C':
+       console.log("C is coorect");
+       break;
+    default:
+       console.log("Not a real answer.");
+
+}
---
+++ javascript/ex7.js
@@ -0,0 +1,8 @@
+let total = 0;
+
+let numArray = [10, 20, 30, 40, 50, 60, 70, 80];
+for (let i = 0; i <numArray.length; i++) {
+
+    total += numArray[i];
+}
+console.log(total);
